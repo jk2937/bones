77adaf599cd49add8b4f18329fa4a11ba13c470b
jk2937 <kaschakjonathan+github@gmail.com>
2024-03-11 14:14:20 -0700

    Refactor bones_assets.js, change Object() constructor to object literal

--- frontend/script/bones_assets.js
+++ frontend/script/bones_assets.js
@@ -1,8 +1,9 @@
-Bones.Assets = new Object();
-
-Bones.Assets.gfx_player = new Image();
-Bones.Assets.gfx_player.src = "../assets/asset2.png";
-
-Bones.Assets.gfx_ball = new Image();
-Bones.Assets.gfx_ball.src = "../assets/asset1.png";
-
+Bones.Assets = {
+    init: function(){
+        this.gfx_player = new Image();
+        this.gfx_ball = new Image();
+        this.gfx_player.src = "../assets/asset2.png";
+        this.gfx_ball.src = "../assets/asset1.png";
+    }
+}
+Bones.Assets.init()
