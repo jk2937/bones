2898b38f3e2bd2784f50c3d4cbc093e0fbb3a503
jk2937 <kaschakjonathan+github@gmail.com>
2024-03-11 14:39:02 -0700

    Refactor bones_timer.js, change Object() constructor to object literal

--- frontend/script/bones_timer.js
+++ frontend/script/bones_timer.js
@@ -1,21 +1,23 @@
-Bones.Timer = new Object()
+Bones.Timer = {
+    init: function() {
+        this.frame_lock = false;
+        // Bones.main_loop_sleep = 0;
+        this.total_lag_frames = 0;
+        this.fps_frame_counter = 0;
+        this.fps = 0;
 
-Bones.Timer.frame_lock = false;
-// Bones.main_loop_sleep = 0;
-Bones.Timer.total_lag_frames = 0;
-Bones.Timer.fps_frame_counter = 0;
-Bones.Timer.fps = 0;
+        this.current_frame_start_time = Date.now();
+        this.previous_frame_start_time = Date.now();
 
-Bones.Timer.current_frame_start_time = Date.now();
-Bones.Timer.previous_frame_start_time = Date.now();
+        this.delta_time = 0;
 
-Bones.Timer.delta_time = 0;
+        this.timescale = 60 / 1000;
+        this.physics_timescale = 1;
 
-Bones.Timer.timescale = 60 / 1000;
-Bones.Timer.physics_timescale = 1;
-
-Bones.Timer.calculate_fps = function() {
-    Bones.Timer.fps = Bones.Timer.fps_frame_counter
-    Bones.Timer.fps_frame_counter = 0
+        this.calculate_fps = function() {
+            this.fps = this.fps_frame_counter
+            this.fps_frame_counter = 0
+        }
+    }
 }
-
+Bones.Timer.init()
