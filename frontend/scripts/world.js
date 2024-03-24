96d08e242a1382f0e481ff29cbdc3be6761855c2
jk2937 <kaschakjonathan+github@gmail.com>
2024-03-23 19:50:34 -0700

    Refactor: move Player and BoxProp inside of World

--- frontend/scripts/world.js
+++ frontend/scripts/world.js
@@ -1,191 +1,10 @@
-Bones.Physics = new Object()
-Bones.Physics.matterjs_engine = Matter.Engine.create();
-Bones.Physics.matterjs_world = Bones.Physics.matterjs_engine.world;
-class BoxProp {
-    constructor(box, angle, anchored) {
-        this.x = box.x
-        this.y = box.y
-
-        this.angle = angle;
-
-        this.w = box.width
-        this.h = box.height
-
-        this.anchored = anchored;
-
-        this.body = Matter.Bodies.rectangle(this.x, this.y, this.w, this.h, {
-            isStatic: this.anchored
-        })
-        Matter.Composite.add(Bones.Physics.matterjs_world, [this.body]);
-    }
-    render() {
-        this.pos = this.body.position;
-        this.angle = this.body.angle;
-        this.x = this.pos.x;
-        this.y = this.pos.y;
-        Bones.Renderer.context.save()
-        Bones.Renderer.context.fillStyle = "white";
-        Bones.Renderer.context.translate(this.x - Bones.Renderer.camera_x, this.y - Bones.Renderer.camera_y);
-        Bones.Renderer.context.rotate(this.angle);
-
-        Bones.Renderer.context.fillRect(0 - this.w / 2, 0 - this.h / 2, this.w, this.h);
-        Bones.Renderer.context.restore()
-    }
-}
-class Player {
-    constructor() {
-
-        //player init
-
-        this.movement_speed = 1;
-        this.x = 25;
-        this.y = 0;
-        this.x_vel = 0;
-        this.y_vel = 0;
-        this.max_x_vel = 7
-        this.max_y_vel = 70
-        this.x_acc = 1
-        this.y_acc = 10 // Todo: air acceleration, air max vel, run speed, bunny hopping
-        this.ground_friction = 0.55
-        this.air_friction = 0.05
-        this.gravity = 0.25
-        this.facing_right = true
-
-		this.move_left = false;
-		this.move_right = false;
-		this.move_jump = false;
-
-        this.on_ground = false
-        this.jump_lock = false
-        this.physics_prop = new BoxProp(new Box(this.x, this.y, 150, 150), 0, true)
-    } 
-	read_keyboard_controls() {
-        this.move_left = Bones.Input.Keyboard.ControlStates["left"].pressed || Bones.Input.Keyboard.ControlStates["left"].pressed_this_frame;
-        this.move_right = Bones.Input.Keyboard.ControlStates["right"].pressed || Bones.Input.Keyboard.ControlStates["right"].pressed_this_frame;
-        this.move_jump = Bones.Input.Keyboard.ControlStates["jump"].pressed || Bones.Input.Keyboard.ControlStates["jump"].pressed_this_frame;
-        if (this.move_left == true && this.move_right == true) {
-            this.move_left = false;
-            this.move_right = false;
-        }
-	}
-	tick() {
-		// Control
-
-		if (this.move_left) {
-			this.x_vel -= this.x_acc * Bones.Timer.delta_time * Bones.Timer.timescale;
-		}
-		if (this.move_right) {
-			this.x_vel += this.x_acc * Bones.Timer.delta_time * Bones.Timer.timescale;
-		}
-		if (this.move_jump && !this.jump_lock && this.on_ground) {
-			this.y_vel -= this.y_acc
-			this.on_ground = false
-			this.jump_lock = true
-		}
-		if (!this.move_jump) {
-			this.jump_lock = false
-		}
-
-		// Gravity
-
-		this.y_vel += this.gravity * Bones.Timer.delta_time * Bones.Timer.timescale
-
-		// Friction
-
-		if (!this.move_left && !this.move_right) {
-			if (this.x_vel > 0) {
-				if (this.on_ground == true) {
-					if (this.x_vel <= this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
-						this.x_vel = 0; // player_ground_friction;
-					} else {
-						this.x_vel -= this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
-					}
-				}
-				if (this.on_ground == false) {
-					if (this.x_vel < this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
-						this.x_vel = this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
-					} else {
-						this.x_vel -= this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
-					}
-				}
-			}
-			if (this.x_vel < 0) {
-				if (this.on_ground == true) {
-					if (this.x_vel >= -this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
-						this.x_vel = 0; // -player_ground_friction
-					} else {
-						this.x_vel += this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
-					}
-				}
-				if (this.on_ground == false) {
-					if (this.x_vel > -this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
-						this.x_vel = -this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale
-					} else {
-						this.x_vel += this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale
-					}
-				}
-			}
-		}
-
-
-		// Maximum Velocities
-
-		if (this.x_vel > this.max_x_vel) {
-			this.x_vel = this.max_x_vel
-		}
-		if (this.x_vel < -this.max_x_vel) {
-			this.x_vel = -this.max_x_vel
-		}
-		if (this.y_vel > this.max_y_vel) {
-			this.y_vel = this.max_y_vel
-		}
-		if (this.y_vel < -this.max_y_vel) {
-			this.y_vel = -this.max_y_vel
-		}
-
-		// Commit x and y Velocities
-
-		this.x += this.x_vel * Bones.Timer.delta_time * Bones.Timer.timescale;
-		this.y += this.y_vel * Bones.Timer.delta_time * Bones.Timer.timescale;
-
-
-		// Movement Bounderies
-
-		if (this.x < 0) {
-			this.x = 0
-			this.x_vel = 0
-		}
-		if (this.x > Bones.Renderer.width - 150) {
-			this.x = Bones.Renderer.width - 150
-			this.x_vel = 0
-		}
-		if (this.y < 0) {
-			this.y = 0
-			this.y_vel = 0 - this.y_vel
-			if (this.y_vel > 0) {
-				this.y_vel = 0
-			}
-		}
-		if (this.y > Bones.Renderer.height - 150) {
-			this.y = Bones.Renderer.height - 150
-			this.y_vel = 0
-			this.on_ground = true
-		}
-
-		Matter.Body.setPosition(this.physics_prop.body, {
-			x: this.x + 150 / 2,
-			y: this.y + 150 / 2
-		}, true)
-	}
-	render() {
-        Bones.Renderer.context.drawImage(Bones.Assets.gfx_player, this.x - Bones.Renderer.camera_x, this.y - Bones.Renderer.camera_y, 150, 150)
-	}
-}
 Bones.World = {
     init() {
-        //player init
+        this.Physics = new Object()
+        this.Physics.matterjs_engine = Matter.Engine.create();
+        this.Physics.matterjs_world = this.Physics.matterjs_engine.world;
 
-        this.player1 = new Player()
+        this.player1 = new this.Player()
 
         this.menu_items = []
         this.menu_items.push(new MenuItem(Bones.Renderer.width - 280, 50, 250, 300, "Menu", function() {}))
@@ -201,9 +20,9 @@ Bones.World = {
 
         // prop init
 
-        this.test_prop1 = new BoxProp(new Box(400, 200, 80, 80), 0, false)
-        this.test_prop2 = new BoxProp(new Box(450, 50, 80, 20), 0, false)
-        this.ground1 = new BoxProp(new Box(400, 610, 810, 60), 0, true)
+        this.test_prop1 = new this.BoxProp(new Box(400, 200, 80, 80), 0, false)
+        this.test_prop2 = new this.BoxProp(new Box(450, 50, 80, 20), 0, false)
+        this.ground1 = new this.BoxProp(new Box(400, 610, 810, 60), 0, true)
 
     },
     tick() {
@@ -247,14 +66,14 @@ Bones.World = {
 
         this.npc1.tick()
 
-        Matter.Engine.update(Bones.Physics.matterjs_engine, Bones.Timer.delta_time)
+        Matter.Engine.update(this.Physics.matterjs_engine, Bones.Timer.delta_time)
 
         this.test_prop1.render()
         this.test_prop2.render()
         this.ground1.render()
 
         if (Bones.DebugDisplay.physics_wireframe == true) {
-            var bodies = Matter.Composite.allBodies(Bones.Physics.matterjs_engine.world)
+            var bodies = Matter.Composite.allBodies(this.Physics.matterjs_engine.world)
 
             Bones.Renderer.context.save()
 
@@ -307,6 +126,186 @@ Bones.World = {
                 Bones.Renderer.context.fillText("", 0, 0)
             }
         }
-    } // END FUNCTION tick
+    }, // END FUNCTION tick
+    BoxProp: class {
+        constructor(box, angle, anchored) {
+            this.x = box.x
+            this.y = box.y
+
+            this.angle = angle;
+
+            this.w = box.width
+            this.h = box.height
+
+            this.anchored = anchored;
+
+            this.body = Matter.Bodies.rectangle(this.x, this.y, this.w, this.h, {
+                isStatic: this.anchored
+            })
+            Matter.Composite.add(Bones.World.Physics.matterjs_world, [this.body]);
+        }
+        render() {
+            this.pos = this.body.position;
+            this.angle = this.body.angle;
+            this.x = this.pos.x;
+            this.y = this.pos.y;
+            Bones.Renderer.context.save()
+            Bones.Renderer.context.fillStyle = "white";
+            Bones.Renderer.context.translate(this.x - Bones.Renderer.camera_x, this.y - Bones.Renderer.camera_y);
+            Bones.Renderer.context.rotate(this.angle);
+
+            Bones.Renderer.context.fillRect(0 - this.w / 2, 0 - this.h / 2, this.w, this.h);
+            Bones.Renderer.context.restore()
+        }
+    }, // END CLASS BoxProp
+    Player: class {
+        constructor() {
+
+            //player init
+
+            this.movement_speed = 1;
+            this.x = 25;
+            this.y = 0;
+            this.x_vel = 0;
+            this.y_vel = 0;
+            this.max_x_vel = 7
+            this.max_y_vel = 70
+            this.x_acc = 1
+            this.y_acc = 10 // Todo: air acceleration, air max vel, run speed, bunny hopping
+            this.ground_friction = 0.55
+            this.air_friction = 0.05
+            this.gravity = 0.25
+            this.facing_right = true
+
+            this.move_left = false;
+            this.move_right = false;
+            this.move_jump = false;
+
+            this.on_ground = false
+            this.jump_lock = false
+            this.physics_prop = new Bones.World.BoxProp(new Box(this.x, this.y, 150, 150), 0, true)
+        } 
+        read_keyboard_controls() {
+            this.move_left = Bones.Input.Keyboard.ControlStates["left"].pressed || Bones.Input.Keyboard.ControlStates["left"].pressed_this_frame;
+            this.move_right = Bones.Input.Keyboard.ControlStates["right"].pressed || Bones.Input.Keyboard.ControlStates["right"].pressed_this_frame;
+            this.move_jump = Bones.Input.Keyboard.ControlStates["jump"].pressed || Bones.Input.Keyboard.ControlStates["jump"].pressed_this_frame;
+            if (this.move_left == true && this.move_right == true) {
+                this.move_left = false;
+                this.move_right = false;
+            }
+        }
+        tick() {
+            // Control
+
+            if (this.move_left) {
+                this.x_vel -= this.x_acc * Bones.Timer.delta_time * Bones.Timer.timescale;
+            }
+            if (this.move_right) {
+                this.x_vel += this.x_acc * Bones.Timer.delta_time * Bones.Timer.timescale;
+            }
+            if (this.move_jump && !this.jump_lock && this.on_ground) {
+                this.y_vel -= this.y_acc
+                this.on_ground = false
+                this.jump_lock = true
+            }
+            if (!this.move_jump) {
+                this.jump_lock = false
+            }
+
+            // Gravity
+
+            this.y_vel += this.gravity * Bones.Timer.delta_time * Bones.Timer.timescale
+
+            // Friction
+
+            if (!this.move_left && !this.move_right) {
+                if (this.x_vel > 0) {
+                    if (this.on_ground == true) {
+                        if (this.x_vel <= this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
+                            this.x_vel = 0; // player_ground_friction;
+                        } else {
+                            this.x_vel -= this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
+                        }
+                    }
+                    if (this.on_ground == false) {
+                        if (this.x_vel < this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
+                            this.x_vel = this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
+                        } else {
+                            this.x_vel -= this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
+                        }
+                    }
+                }
+                if (this.x_vel < 0) {
+                    if (this.on_ground == true) {
+                        if (this.x_vel >= -this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
+                            this.x_vel = 0; // -player_ground_friction
+                        } else {
+                            this.x_vel += this.ground_friction * Bones.Timer.delta_time * Bones.Timer.timescale;
+                        }
+                    }
+                    if (this.on_ground == false) {
+                        if (this.x_vel > -this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale) {
+                            this.x_vel = -this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale
+                        } else {
+                            this.x_vel += this.air_friction * Bones.Timer.delta_time * Bones.Timer.timescale
+                        }
+                    }
+                }
+            }
+
+
+            // Maximum Velocities
+
+            if (this.x_vel > this.max_x_vel) {
+                this.x_vel = this.max_x_vel
+            }
+            if (this.x_vel < -this.max_x_vel) {
+                this.x_vel = -this.max_x_vel
+            }
+            if (this.y_vel > this.max_y_vel) {
+                this.y_vel = this.max_y_vel
+            }
+            if (this.y_vel < -this.max_y_vel) {
+                this.y_vel = -this.max_y_vel
+            }
+
+            // Commit x and y Velocities
+
+            this.x += this.x_vel * Bones.Timer.delta_time * Bones.Timer.timescale;
+            this.y += this.y_vel * Bones.Timer.delta_time * Bones.Timer.timescale;
+
+
+            // Movement Bounderies
+
+            if (this.x < 0) {
+                this.x = 0
+                this.x_vel = 0
+            }
+            if (this.x > Bones.Renderer.width - 150) {
+                this.x = Bones.Renderer.width - 150
+                this.x_vel = 0
+            }
+            if (this.y < 0) {
+                this.y = 0
+                this.y_vel = 0 - this.y_vel
+                if (this.y_vel > 0) {
+                    this.y_vel = 0
+                }
+            }
+            if (this.y > Bones.Renderer.height - 150) {
+                this.y = Bones.Renderer.height - 150
+                this.y_vel = 0
+                this.on_ground = true
+            }
+
+            Matter.Body.setPosition(this.physics_prop.body, {
+                x: this.x + 150 / 2,
+                y: this.y + 150 / 2
+            }, true)
+        }
+        render() {
+            Bones.Renderer.context.drawImage(Bones.Assets.gfx_player, this.x - Bones.Renderer.camera_x, this.y - Bones.Renderer.camera_y, 150, 150)
+        }
+    }, // END CLASS Player
 } // END OBJECT Bones.World
 Bones.World.init()
