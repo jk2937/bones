0a447b610507f97dc270e53eab478bb6c3570554
jk2937 <kaschakjonathan+github@gmail.com>
2024-05-14 11:58:52 -0400

    Add netplay player movement

--- frontend/scripts/bones_netplay.js
+++ frontend/scripts/bones_netplay.js
@@ -4,4 +4,16 @@ function activate_netplay_controller () {
     netplay_controller = true
 
     console.log('neplay controller activated')
-}
+
+
+    }
+
+socket.on('control state message', function(msg) {
+    console.log('recieved control state message')
+        console.log(msg.move_jump)
+        Bones.World.player1.move_left = msg.move_left;
+        Bones.World.player1.move_right = msg.move_right;
+        Bones.World.player1.move_jump = msg.move_jump;
+    });
+
+activate_netplay_controller()
