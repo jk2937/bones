4a7574b6cc8dc7be4e57fb264e63b74f894e4b4c
Jonathan Kaschak <131540356+jk2937@users.noreply.github.com>
2025-02-02 16:01:30 -0500

    improve bullet hit detection

--- public/scripts/world.js
+++ public/scripts/world.js
@@ -526,8 +526,8 @@ Bones.World = {
 				for (let j = 0; j < this.bullets.length; j++) {
 					if(this.players[i].id != this.bullets[j].owner/* || this.bullets[j].size > 70*/) {
 						if (circle_collision(
-						this.players[i].x + this.players[i].width / 2, 
-						this.players[i].y + this.players[i].height / 2,
+						this.players[i].x_interp_calc + this.players[i].width / 2, 
+						this.players[i].y_interp_calc + this.players[i].height / 2,
 						this.players[i].width / 2,
 						this.bullets[j].x_interp_calc + this.bullets[j].size / 2,
 						this.bullets[j].y_interp_calc + this.bullets[j].size / 2,
@@ -677,7 +677,7 @@ Bones.World = {
 			Bones.Renderer.context.font = "bold 24px Monospace";
 			Bones.Renderer.context.fillStyle = "#495664";
 			Bones.Renderer.context.textAlign = "center";
-			Bones.Renderer.context.fillText("Welcome to Project Bones Alpha branch-network_testing-0.3.7!", Bones.Renderer.canvas.width / 2, 25)
+			Bones.Renderer.context.fillText("Welcome to Project Bones Alpha branch-network_testing-0.3.8!", Bones.Renderer.canvas.width / 2, 25)
 			
 			for (let i = 0; i < this.players.length; i++) {
 				Bones.Renderer.context.font = "bold 24px Monospace";
