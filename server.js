8552f2c692119a70d57f846b0b9a9397f4461b74
Jonathan Kaschak <kaschakjonathan+github@gmail.com>
2025-02-23 19:41:46 +0000

    Change port

--- server.js
+++ server.js
@@ -111,6 +111,6 @@ io.on('connection', (socket) => {
 	});
 });
 
-http.listen(3000, () => {
-	console.log('Server running on port 3000');
+http.listen(3047, () => {
+	console.log('Server running on port 3047');
 });
